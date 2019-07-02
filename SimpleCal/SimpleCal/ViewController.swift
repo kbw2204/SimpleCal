cea40131dde977649029b129c3aceb762d0be711
강병우 <kbw2204@gmail.com>
2019-07-02 18:38:58 +0900

    Initial commit

---
+++ SimpleCal/SimpleCal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCal
+//
+//  Created by 강병우 on 02/07/2019.
+//  Copyright © 2019 강병우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
