bb166c529fbb140273e2cff5901d8fa6b37c8604
강병우 <kbw2204@gmail.com>
2019-07-04 17:40:49 +0900

    add calendarView edit scrollmode

---
+++ SimpleCal/SimpleCal/Model/DateCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateCell.swift
+//  SimpleCal
+//
+//  Created by 강병우 on 04/07/2019.
+//  Copyright © 2019 강병우. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class DateCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
+    
+}
